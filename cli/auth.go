6053f1cb40cbabfd938ad98d1fc2c18bd9ee16f5
Mirko Mälicke <mirko@hydrocode.de>
2025-04-17 21:32:51 +0200

    feat: enhance authentication CLI commands and add new dependencies
    - Introduced new commands for managing admin credentials and creating users in the CLI.
    - Integrated `github.com/jedib0t/go-pretty/v6` for improved table formatting in the credentials display.
    - Added `github.com/mattn/go-runewidth` and `github.com/rivo/uniseg` as indirect dependencies to support new features.
    - Removed unused imports and streamlined the authentication command structure for better clarity.

--- cli/auth.go
+++ cli/auth.go
@@ -1,6 +1,22 @@
 package cli
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/hydrocode-de/gorun/internal/auth"
+	"github.com/hydrocode-de/gorun/internal/db"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	refreshToken bool
+	accessToken  bool
+	isAdmin      bool
+	password     string
+)
 
 var authCmd = &cobra.Command{
 	Use:   "auth",
@@ -10,6 +26,58 @@ var authCmd = &cobra.Command{
 	},
 }
 
+var credentialsCmd = &cobra.Command{
+	Use:   "credentials",
+	Short: "Show Admin credentials for GoRun",
+	Run: func(cmd *cobra.Command, args []string) {
+		credentials, err := auth.GetAdminCredentials(cmd.Context())
+		cobra.CheckErr(err)
+
+		if refreshToken {
+			fmt.Println(credentials.RefreshToken)
+			return
+		}
+
+		if accessToken {
+			fmt.Println(credentials.AccessToken)
+			return
+		}
+
+		t := table.NewWriter()
+		t.SetStyle(table.StyleColoredBlackOnBlueWhite)
+		t.SetOutputMirror(os.Stdout)
+
+		t.AppendHeader(table.Row{"Key", "Value"})
+		t.AppendRow(table.Row{"Email", credentials.Email})
+		t.AppendRow(table.Row{"User ID", credentials.UserID})
+		t.AppendRow(table.Row{"Refresh token", credentials.RefreshToken})
+		t.AppendRow(table.Row{"Expires at", credentials.ExpiresAt})
+		t.AppendRow(table.Row{"Access token", credentials.AccessToken})
+
+		t.Render()
+	},
+}
+
+var createUserCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a new user",
+	Run: func(cmd *cobra.Command, args []string) {
+		DB := viper.Get("db").(*db.Queries)
+		secret := viper.GetString("secret")
+
+		_, err := auth.CreateUser(cmd.Context(), DB, args[0], password, isAdmin, secret)
+		cobra.CheckErr(err)
+	},
+}
+
 func init() {
+	credentialsCmd.Flags().BoolVar(&refreshToken, "refresh-token", false, "Show only the refresh token")
+	credentialsCmd.Flags().BoolVar(&accessToken, "access-token", false, "Show only the access token")
+
+	createUserCmd.Flags().BoolVar(&isAdmin, "admin", false, "Create an admin user")
+	createUserCmd.Flags().StringVar(&password, "password", "", "The password for the new user. If you don't provide one, the user can't login.")
+
+	authCmd.AddCommand(credentialsCmd)
+	authCmd.AddCommand(createUserCmd)
 	rootCmd.AddCommand(authCmd)
 }
--- go.mod
+++ go.mod
@@ -29,7 +29,9 @@ require (
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jedib0t/go-pretty/v6 v6.6.7 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/term v0.5.2 // indirect
@@ -40,6 +42,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
