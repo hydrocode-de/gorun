bf8d1e1cab5b0b76fc2c93022cf8bf8a3b3583cc
Mirko Mälicke <mirko@hydrocode.de>
2025-04-17 21:32:51 +0200

    feat: enhance authentication CLI commands and add new dependencies
    - Introduced new commands for managing admin credentials and creating users in the CLI.
    - Integrated `github.com/jedib0t/go-pretty/v6` for improved table formatting in the credentials display.
    - Added `github.com/mattn/go-runewidth` and `github.com/rivo/uniseg` as indirect dependencies to support new features.
    - Removed unused imports and streamlined the authentication command structure for better clarity.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/hydrocode-de/gorun/cli"
@@ -10,12 +9,6 @@ import (
 func main() {
 	cli.Execute()
 	os.Exit(0)
-
-	// DEV section
-	// read in a argv for a docker image name
-	if len(os.Args) == 1 {
-		log.Println("No command line arguments provided")
-	}
 
 	// if os.Args[1] == "find" {
 	// 	if len(os.Args) < 3 {
@@ -31,40 +24,4 @@ func main() {
 	// 	}
 	// }
 
-	// if os.Args[1] == "auth" {
-	// 	if len(os.Args) < 3 {
-	// 		log.Fatal("missing command - use like gorun auth create")
-	// 	}
-
-	// 	if os.Args[2] == "credentials" {
-	// 		credentials, err := config.GetAdminCredentials()
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		}
-
-	// 		if len(os.Args) > 2 && os.Args[2] == "--token" {
-	// 			fmt.Printf("%s\n", credentials.RefreshToken)
-	// 			return
-	// 		}
-	// 		fmt.Println("Admin credentials:")
-	// 		fmt.Printf("Email:      %s\n", credentials.Email)
-	// 		fmt.Printf("User ID:       %s\n", credentials.UserID)
-	// 		fmt.Printf("Refresh token: %s\n", credentials.RefreshToken)
-	// 		fmt.Printf("Expires at:    %s\n", credentials.ExpiresAt)
-	// 		fmt.Printf("Access token  \n\n%s\n", credentials.AccessToken)
-	// 	}
-
-	// 	if os.Args[2] == "create-user" {
-	// 		if len(os.Args) < 6 {
-	// 			log.Fatal("missing arguments - use like gorun auth create-user <email> <password> <is-admin>")
-	// 		}
-
-	// 		_, err := auth.CreateUser(context.Background(), config.GetDB(), os.Args[3], os.Args[4], os.Args[5] == "true", config.Secret)
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		}
-
-	// 		log.Println("User created")
-	// 	}
-	// }
 }
